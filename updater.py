22e774f192c18b2c6f4e5b4b4f8bc10f33d7d053
Stavros Korokithakis <hi@stavros.io>
2024-02-26 13:14:36 +0200

    Add updater

---
+++ updater.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+"""
+Update all LinuxServer containers to their latest versions.
+"""
+import re
+
+import requests
+
+url = "https://fleet.linuxserver.io/api/v1/images"
+response = requests.get(url)
+data = response.json()
+versions = {
+    program["name"]: program["version"]
+    for program in data["data"]["repositories"]["linuxserver"]
+}
+
+with open("docker-compose.yml") as infile:
+    compose = infile.read()
+programs = re.findall("lscr.io/linuxserver/(.*?):.*$", compose, re.MULTILINE)
+for program in programs:
+    version = versions[program]
+    print(program, version)
+    compose = re.sub(
+        f"lscr.io/linuxserver/{program}:.*$",
+        f"lscr.io/linuxserver/{program}:{version}",
+        compose,
+        flags=re.MULTILINE,
+    )
+
+with open("docker-compose.yml", "w") as outfile:
+    outfile.write(compose)
